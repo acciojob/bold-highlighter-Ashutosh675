a08950171d676bc3ca7a605790d16e8af61a3b06
Ashutosh singh <75128637+Ashutosh675@users.noreply.github.com>
2023-07-24 15:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function highlight() {
     //Write your code here
+	function highlight() {
+    //Write your code here
+  var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'green';
+  });
+}
+function return_normal() {
+   var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'black';
+  });
+}
 
 
 }
